94f706a8938aed9f365f35565ef74eba36af44c7
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-28 21:22:58 -0400

    setup Flight::view

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ require_once 'vendor/autoload.php';
 
 $_ENV += (require '.env.php') + (require '.env.dist.php');
 
+Flight::set('flight.views.path', 'resources/views');
+Flight::view()->preserveVars = false;
+
 Flight::start();
