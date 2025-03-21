d3c7ab7836d8f370ff280dc7d5b8a15cdc141fcb
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-21 19:03:51 -0400

    remove unused v1 api group

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ declare(strict_types=1);
 
 Flight::group('/api', static function (): void {
   Flight::route('GET /status', static fn() => Flight::json(['status' => 'ok']));
-
-  Flight::group('/v1', static function (): void {
-    //
-  });
 });
